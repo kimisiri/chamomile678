b366231754b84c99c5eb7a9836b28577c5945192
meh <siri.nga@student.mahidol.edu>
2025-10-07 23:50:21 +0700

    tried

--- idbind.js
+++ idbind.js
@@ -1,6 +1,66 @@
 
 const inpbox = document.getElementById('student-id');
 
+function getCookie(query) {
+  const cookie_now = document.cookie.split("; ");
+  for (let i = 0; i < cookie_now.length; i++) {
+    let str = cookie_now[i].split("=");
+    let key = str[0];
+    let value = str[1];
+    if (key === query) {
+      if (!isNaN(Number(value))) {
+        value = Number(value);
+      }
+      return value;
+    }
+  }
+  return null;
+}
+let stid = getCookie("");
+if (stid != null && stid != "") {
+  window.location.href = "./";
+}
+
+function queryServer(userid) {
+  fetch("https://script.google.com/macros/s/AKfycby-QILzuttyKoPwVPwS_jhGvDc7um4pc-TwXpH3zkGRI96_TKXuNy0QLJbxxKTiGNYK/exec", {
+      method: "POST",
+      body: JSON.stringify({
+        sessionId: getCookie("session_id"),
+        studentId: userid
+      }),
+      headers: {
+        "Content-Type": "text/plain"
+    }
+  }).then((result) => {
+    loadingToast.hideToast()
+    if (!result.ok) {
+      Toastify({
+        text: "Server Error : Endpoint error " + result.statusText,
+        duration: 3000,
+        close: false,
+        gravity: "bottom",
+        position: "center",
+        backgroundColor: "#ff00009a"
+      }).showToast();
+      throw new Error('Server response error: ' + result.statusText);
+    }
+    return result.json();
+  }).then((json) => {
+    if (json.success) {
+      document.cookie = "student_id="+userid+";";
+      window.location.href = "./";
+    } else {
+      // something is wrong so just relogin lmao
+      document.cookie = "session_id='';expires=Thu, 01 Jan 1970 00:00:01 GMT";
+      reasoning = "Session expired, try logging in again.";
+      window.location.href = "./login.html";
+    }
+  })
+}
+
+let studentidrn = null;
+
+
 const popup = new Popup({
   id: "confirmationpopup",
   title: "Confirm your student id.",
@@ -13,7 +73,17 @@ const popup = new Popup({
   loadCallback: function() {
     const root = document;
     root.querySelector(".refuse")?.addEventListener("click", () => popup.hide());
-    root.querySelector(".accept")?.addEventListener("click", () => popup.hide());
+    root.querySelector(".accept")?.addEventListener("click", () => {
+      Toastify({
+        text: "Processing request...",
+        duration: -1,
+        close: false,
+        gravity: "bottom",
+        position: "center",
+        backgroundColor: "#6464649a"
+      }).showToast();
+      queryServer(userin);
+    });
   }
 });
 function makepopup(userin) {
@@ -26,6 +96,7 @@ function handleInput() {
   const userin = inpbox.value;
   console.log(userin);
   if (pattern.test(userin)) {
+    studentidrn = userin;
     makepopup(userin)
   } else {
     Toastify({
